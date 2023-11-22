6ec8a017c4b65cae3e6c85281d4a776a7d5a5ac2
Ingo Boehme <ingo.boehme@zuehlke.com>
2023-11-22 22:09:48 +0100

    Added API and groundlaying REST service

--- FHNW-REST-Service/DemoPeople.Model.swift
+++ FHNW-REST-Service/DemoPeople.Model.swift
@@ -1,14 +1,55 @@
-// Created 22.11.2023 
+// Created 22.11.2023
 
 import Foundation
 extension DemoPeopleView {
+    enum JSONPlaceholderAPI {
+        case users
+        case todo
+
+        // MARK: Internal
+
+        var url: URL? {
+            switch self {
+            case .users:
+                URL(string: "https://jsonplaceholder.typicode.com/users")
+            case .todo:
+                URL(string: "https://jsonplaceholder.typicode.com/todos")
+            }
+        }
+    }
     
-    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
     
     class Model {
         let restService = RestService()
-    }
-    
-    
-}
 
+        func load(method: HTTPMethod, url: URL) {
+            var request = URLRequest(url: url)
+            request.httpMethod = method.rawValue
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                guard let response,
+                      let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                      statusCode == 200
+                else {
+                    print("HTTP Status is NOT 200")
+                    return
+                }
+                
+                guard let data else {
+                    print("Data is empty")
+                    return
+                }
+            }
+        }
+    }
+}
--- FHNW-REST-Service/REST-Service.swift
+++ FHNW-REST-Service/REST-Service.swift
@@ -5,4 +5,5 @@ import Foundation
 class RestService {
     
     
+    
 }
