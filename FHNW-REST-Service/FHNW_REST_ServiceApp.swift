43b3b2efb8bdef5d6f001371ed640fb63275bcbe
Ingo Boehme <ingo.boehme@zuehlke.com>
2023-11-22 21:21:37 +0100

    Initial Commit

---
+++ FHNW-REST-Service/FHNW_REST_ServiceApp.swift
@@ -0,0 +1,12 @@
+// Created 22.11.2023 
+
+import SwiftUI
+
+@main
+struct FHNW_REST_ServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
